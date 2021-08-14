585c4ee930b5a84acbabcb6fbb239e4e8d029153
Hnodomar <bluesilverg22@gmail.com>
2021-08-14 20:26:59 +0100

    logging

--- policyTests.cpp
+++ policyTests.cpp
@@ -6,6 +6,7 @@
 #include <cctype>
 #include <string_view>
 #include <string>
+#include <fstream>
 
 std::string setupTestString(const int& length) {
     std::string ret(length, 'a');
@@ -38,23 +39,43 @@ std::size_t countWords(std::string_view test_str, ExecutionPolicy policy) {
 }
 
 template<typename AlgoType>
-void measureTest(const char* test_name, AlgoType run_test) {
+double measureTest(const char* test_name, AlgoType run_test) {
     const auto start_time = std::chrono::steady_clock::now();
     std::size_t ret = run_test();
     const auto end_time = std::chrono::steady_clock::now();
     std::cout << test_name << ": " 
         << std::chrono::duration<double, std::milli>(end_time - start_time).count()
         << " ms, res " << ret << std::endl;
+    return std::chrono::duration<double, std::milli>(end_time - start_time).count();
+}
+
+void runIterationsAndOutputData(int max_length, int step_size) {
+    int str_length = step_size;
+    while (str_length < max_length) {
+        std::string test_str(setupTestString(str_length));
+        std::fstream data_file;
+        data_file.open("output.dat", std::ios_base::app);
+        data_file << str_length << " ";
+        data_file << measureTest("wordCount seq", [&test_str]{
+            return countWords(test_str, std::execution::seq);
+        }) << " ";
+        data_file << measureTest("wordCount par", [&test_str]{
+            return countWords(test_str, std::execution::par);
+        }) << " ";
+        data_file << measureTest("wordCoun par_unseq", [&test_str]{
+            return countWords(test_str, std::execution::par_unseq);
+        }) << std::endl;
+        str_length += step_size;
+    }
 }
 
 int main(int argc, char** argv) {
-    const int len = argc > 1 ? atoi(argv[1]) : 1;
-    if (len == 1) {
-        printf("Usage: parallelWordCount <len>\n");
+    if (argc < 3 || argc > 3) {
+        std::cout << "Usage: ./policyTests <iteration_max> <step_size>\n";
         return 1;
     }
-    std::string test_str(setupTestString(len));
-    measureTest("word_count seq", [&test_str]{
-        return countWords(test_str, std::execution::seq);
-    });
+    int iteration_max = std::atoi(argv[1]);
+    int step_size = std::atoi(argv[2]);
+    runIterationsAndOutputData(100000000, 10000000);
+    return 0;
 }
